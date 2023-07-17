4958859e1c5d552d7fc187144afcf24d069f8f1d
Smart3258 <Olarinoyeopeyemi81@gmail.com>
2023-07-17 06:15:00 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,16 @@ int num1;
 int num2;
 for (num1 = 0; num1 < 99; num1++)
 {
-for (num2 = num1 + 1; num2 <= 99; num2++)
+for (num2 = 0; num2 <= 99; num2++)
+{
+if (num1 < num2)
 {
 putchar((num1 / 10) + 48);
 putchar((num1 % 10) + 48);
 putchar(' ');
 putchar((num2 / 10) + 48);
 putchar((num2 % 10) + 48);
+}
 if (num1 == 99 && num2 == 98)
 continue;
 putchar(',');
